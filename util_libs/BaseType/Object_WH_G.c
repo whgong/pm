146b717dbd769840e72d09c2c1f567937fd24a4f
rake <13s13@163.com>
2014-02-26 11:09:48 +0800

    util_libs basetype 重构20140226

--- util_libs/BaseType/Object_WH_G.c
+++ util_libs/BaseType/Object_WH_G.c
@@ -3,244 +3,124 @@
 #include<stdio.h>
 #include<stdarg.h>
 
-//macro================================================================
-#define CHECK_OBJ_ISINITED(OBJ_STR, RC_STR, RET_STR) \
-    if(!objIsInit(OBJ_STR)){ \
-        RC_STR = RC_OBJ_NOT_INIT; \
-        return RET_STR; \
-    }
-
-#define CHECK_OBJOP_ISINITED(OP_STR, RC_STR, RET_STR) \
-    if(!opIsInit(OP_STR)){ \
-        RC_STR = RC_OBJ_NOT_INIT; \
-        return RET_STR; \
-    }
-
-#define CHECK_OBJ_OBJDEAL(RC_STR, RET_STR) \
-    if(RC_STR != RC_OBJ_SUCCESS){ \
-        RC_STR = RC_OBJ_NOT_INIT; \
-        return RET_STR; \
-    }
-
 //declare==============================================================
-Object_WH_G *Object_WH_G_getInst(RC_OBJ_WH_G *rc);
-void Object_WH_G_Init(Object_WH_G *obj, RC_OBJ_WH_G *rc);
-Obj_Op_WH_G *Object_WH_G_getOpInst(RC_OBJ_WH_G *rc);
-void Obj_Op_WH_G_Init(Obj_Op_WH_G *op, RC_OBJ_WH_G *rc);
-static void Object_WH_G_freeOpInst(Obj_Op_WH_G *op, RC_OBJ_WH_G *rc);
-void Object_WH_G_bindOperation(Object_WH_G *obj, Obj_Op_WH_G *op, RC_OBJ_WH_G *rc);
-static void Object_WH_G_bindSetter(Obj_Op_WH_G *op, RC_OBJ_WH_G *rc, void (*setter)(void **val, ...));
-static void Object_WH_G_bindReleaser(Obj_Op_WH_G *op, RC_OBJ_WH_G *rc, void (*releaser)(void* val));
-static void Object_WH_G_toString(void *thiz, RC_OBJ_WH_G *rc, char* str);
-static short Object_WH_G_compare(void *thiz, RC_OBJ_WH_G *rc, void *aVal);
+Object_WH_G *Obj_WH_G_getInst(int *rc);
+void Obj_WH_G_Init(Object_WH_G *obj, int *rc);
+//Object_Op_WH_G *Obj_WH_G_getOpInst(int *rc);
+void Obj_Op_WH_G_Init(Object_Op_WH_G *op, int *rc);
+static void Object_WH_G_toString(Object_WH_G *thiz, int *rc, char* str);
+static short Object_WH_G_compare(Object_WH_G *thiz, int *rc, Object_WH_G *aVal);
 
-static void Object_WH_G_destory(void *thiz, RC_OBJ_WH_G *rc);
-static void *Object_WH_G_clone(void *thiz, RC_OBJ_WH_G *rc);
-static void Object_WH_G_set(void *thiz, RC_OBJ_WH_G *rc, ...);
-//tool method
-static unsigned char objIsInit(void *obj);
-static unsigned char opIsInit(void *op);
+static void Object_WH_G_destory(Object_WH_G *thiz, int *rc);
+static void *Object_WH_G_clone(Object_WH_G *thiz, int *rc);
+static void Object_WH_G_set(Object_WH_G *thiz, int *rc,...);
 
 //global variable==================================================
-static Obj_Op_WH_G OP_OBJECT_GL={ //全局op
+Object_Op_WH_G GLOP_OBJ_WH_G = { //全局op
     Object_WH_G_destory, //destory
     Object_WH_G_clone, //clone
     Object_WH_G_set, //set
-    NULL, //setter
-    Object_WH_G_bindSetter, //bindSetter
-    NULL, //releaser
-    Object_WH_G_bindReleaser, //bindReleaser
     Object_WH_G_toString, //toString
     Object_WH_G_compare, //compare
-    Object_WH_G_freeOpInst //freeOp
+    NULL,
+    NULL
     };
 
 //implements===========================================================
-void Object_WH_G_Init(Object_WH_G *obj, RC_OBJ_WH_G *rc){
+Object_WH_G *Obj_WH_G_getInst(int *rc){
     *rc = RC_OBJ_SUCCESS;
+    Object_WH_G *res = NULL;
 
-    CHECK_OBJ_ISINITED(obj, *rc, );
-
-    Obj_Op_WH_G_Init(&OP_OBJECT_GL, rc);
-    CHECK_OBJ_OBJDEAL(*rc, );
-
-    obj->val = NULL;
-    obj->op = &OP_OBJECT_GL;
-
-    return;
-}
-
-void Obj_Op_WH_G_Init(Obj_Op_WH_G *op, RC_OBJ_WH_G *rc){
-    *rc = RC_OBJ_SUCCESS;
-
-    if(NULL == op){
-        *rc = RC_OBJ_NOT_INIT;
-        return;
+    res = (Object_WH_G *)malloc(sizeof(Object_WH_G));
+    if(res == NULL){
+        *rc = RC_OBJ_MEMORY_ALLOC_FAIL; return NULL;
     }
 
-    if(op->destory != Object_WH_G_destory){
-        op->destory = Object_WH_G_destory;
-        op->clone = Object_WH_G_clone;
-        op->set = Object_WH_G_set;
-        op->bindSetter = Object_WH_G_bindSetter;
-        op->bindReleaser = Object_WH_G_bindReleaser;
-        op->freeOp = Object_WH_G_freeOpInst;
-        op->setter = NULL;
-        op->releaser = NULL;
-    }
-
-    return;
-}
-
-void Object_WH_G_bindOperation(Object_WH_G *obj, Obj_Op_WH_G *op, RC_OBJ_WH_G *rc){
-    *rc = RC_OBJ_SUCCESS;
-
-    CHECK_OBJ_ISINITED(obj, *rc, );
-    CHECK_OBJOP_ISINITED(op, *rc, );
-
-    obj->op = op;
-
-    return;
-}
-
-Object_WH_G *Object_WH_G_getInst(RC_OBJ_WH_G *rc){
-    *rc = RC_OBJ_SUCCESS;
-    Object_WH_G *obj = NULL;
-    obj = (Object_WH_G *)malloc(sizeof(Object_WH_G));
-    if(NULL == obj){
-        *rc = RC_OBJ_MEMORY_ALLOC_ERROR;
+    Obj_WH_G_Init(res, rc);
+    if(*rc != RC_OBJ_SUCCESS)
         return NULL;
-    }
 
-    Object_WH_G_Init(obj, rc);
-    CHECK_OBJ_OBJDEAL(*rc, NULL)
-
-    return obj;
+    return res;
 }
 
-static void Object_WH_G_destory(void *thiz, RC_OBJ_WH_G *rc){
+void Obj_WH_G_Init(Object_WH_G *obj, int *rc){
     *rc = RC_OBJ_SUCCESS;
-    Object_WH_G *obj = thiz;
 
-    CHECK_OBJ_ISINITED(obj, *rc, );
-    CHECK_OBJOP_ISINITED(obj->op, *rc, );
+    if(obj == NULL){*rc = RC_OBJ_NOT_INIT; return;}
 
-    if(NULL != obj->op->releaser){
-        obj->op->releaser(obj->val);
+    obj->obj_op = &GLOP_OBJ_WH_G;
+    obj->data = NULL;
+
+    return ;
+}
+
+void Obj_Op_WH_G_Init(Object_Op_WH_G *op, int *rc){
+    *rc = RC_OBJ_SUCCESS;
+
+    if(NULL == op){*rc = RC_OBJ_NOT_INIT; return;}
+
+    if(op->destory != GLOP_OBJ_WH_G.destory){
+        op->destory = GLOP_OBJ_WH_G.destory;
+        op->clone = GLOP_OBJ_WH_G.clone;
+        op->set = GLOP_OBJ_WH_G.set;
+        op->toString = GLOP_OBJ_WH_G.toString;
+        op->compare = GLOP_OBJ_WH_G.compare;
+        op->setter = GLOP_OBJ_WH_G.setter;
+        op->releaser = GLOP_OBJ_WH_G.releaser;
+    }
+
+    return ;
+}
+
+static void Object_WH_G_destory(Object_WH_G *thiz, int *rc){
+    *rc = RC_OBJ_SUCCESS;
+
+    if(thiz->obj_op->releaser != NULL){
+        thiz->obj_op->releaser(thiz);
     }
 
     free(thiz);
     return;
 }
 
-static void *Object_WH_G_clone(void *thiz, RC_OBJ_WH_G *rc){
+static void *Object_WH_G_clone(Object_WH_G *thiz, int *rc){
     *rc = RC_OBJ_SUCCESS;
-    Object_WH_G *obj = thiz, *res = NULL;
+    Object_WH_G *res = NULL;
 
-    CHECK_OBJ_ISINITED(obj, *rc, NULL);
-    CHECK_OBJOP_ISINITED(obj->op, *rc, NULL);
+    res = (Object_WH_G*)malloc(sizeof(Object_WH_G));
+    if(res == NULL){*rc = RC_OBJ_MEMORY_ALLOC_FAIL; return NULL;}
 
-    res = (Object_WH_G *)Object_WH_G_getInst(rc);
-    CHECK_OBJ_OBJDEAL(*rc, NULL);
-
-    res->val = obj->val;
-    res->op = obj->op;
+    res->data = thiz->data;
+    res->obj_op = thiz->obj_op;
 
     return res;
 }
 
-static void Object_WH_G_set(void *thiz, RC_OBJ_WH_G *rc, ...){
+static void Object_WH_G_set(Object_WH_G *thiz, int *rc, ...){
     *rc = RC_OBJ_SUCCESS;
-    Object_WH_G *obj = thiz;
     va_list ap;
-
-    CHECK_OBJ_ISINITED(obj, *rc, );
-    CHECK_OBJOP_ISINITED(obj->op, *rc, );
 
     va_start(ap,rc);
 
-    if(NULL != obj->op->setter){
-        obj->op->setter(&(obj->val), *((void**)ap));
+    if(thiz->obj_op->setter != NULL){
+        thiz->obj_op->setter(thiz, *((void **)ap));
     }
 
-    obj->val = *((void**)ap);
+    thiz->data = *((void **)ap);
 
     va_end(ap);
 
     return;
 }
 
-static void Object_WH_G_bindSetter(Obj_Op_WH_G *op, RC_OBJ_WH_G *rc, void (*setter)(void **val, ...)){
-    *rc = RC_OBJ_SUCCESS;
-    CHECK_OBJOP_ISINITED(op, *rc, );
-
-    op->setter = setter;
-
-    return;
-}
-
-static void Object_WH_G_bindReleaser(Obj_Op_WH_G *op, RC_OBJ_WH_G *rc, void (*releaser)(void* val)){
-    *rc = RC_OBJ_SUCCESS;
-    CHECK_OBJOP_ISINITED(op, *rc, );
-
-    op->releaser = releaser;
-
-    return;
-}
-
-static unsigned char objIsInit(void *obj){
-
-    if(NULL == obj){
-        return 0;
-    }
-    if(NULL == ((Object_WH_G*)obj)->op){
-        return 0;
-    }
-    return 1;
-}
-
-static unsigned char opIsInit(void *op){
-    if(NULL == op){
-        return 0;
-    }
-    if(NULL == ((Obj_Op_WH_G*)op)->destory){
-        return 0;
-    }
-    return 1;
-}
-
-Obj_Op_WH_G *Object_WH_G_getOpInst(RC_OBJ_WH_G *rc){
-    *rc = RC_OBJ_SUCCESS;
-    Obj_Op_WH_G *res = NULL;
-
-    res = (Obj_Op_WH_G *)malloc(sizeof(Obj_Op_WH_G));
-    if(NULL == res){
-        *rc = RC_OBJ_MEMORY_ALLOC_ERROR;
-        return NULL;
-    }
-
-    Obj_Op_WH_G_Init(res, rc);
-    CHECK_OBJ_OBJDEAL(*rc, NULL);
-
-    return res;
-}
-
-static void Object_WH_G_freeOpInst(Obj_Op_WH_G *op, RC_OBJ_WH_G *rc){
-    *rc = RC_OBJ_SUCCESS;
-
-    free(op);
-
-    return;
-}
-
-static void Object_WH_G_toString(void *thiz, RC_OBJ_WH_G *rc, char* str){
+static void Object_WH_G_toString(Object_WH_G *thiz, int *rc, char *str){
     *rc = RC_OBJ_SUCCESS;
 
     sprintf(str, "OBJ(@%p)", thiz);
     return;
 }
 
-static short Object_WH_G_compare(void *thiz, RC_OBJ_WH_G *rc, void *aVal){
+static short Object_WH_G_compare(Object_WH_G *thiz, int *rc, Object_WH_G *aVal){
     *rc = RC_OBJ_SUCCESS;
 
     if(thiz == aVal)return 0;
--- util_libs/BaseType/Object_WH_G.h
+++ util_libs/BaseType/Object_WH_G.h
@@ -1,38 +1,39 @@
 #ifndef OBJECT_WH_G_H
 #define OBJECT_WH_G_H
 
-typedef enum _OBJ_RC_13s13_163COM{
+typedef enum _OBJ_RC_13S13_163COM{
     RC_OBJ_NOT_INIT = -3, //未初始化
-    RC_OBJ_MEMORY_ALLOC_ERROR = -2, //内存分配失败
+    RC_OBJ_MEMORY_ALLOC_FAIL = -2, //内存分配失败
     RC_OBJ_ERROR = -1, //未定义的异常
     RC_OBJ_SUCCESS = 0 //成功
 } RC_OBJ_WH_G;
 
 typedef struct __OBJECT_13S13_163_COM_ Object_WH_G;
-typedef struct __OBJECT_OPER_13S13_163_COM_ Obj_Op_WH_G;
+typedef struct __OBJECT_OPER_13S13_163_COM_ Object_Op_WH_G;
+
+extern Object_Op_WH_G GLOP_OBJ_WH_G;
 
 struct __OBJECT_OPER_13S13_163_COM_  { //object opertions
-    void (*destory)(void *thiz, RC_OBJ_WH_G *rc);
-    void *(*clone)(void *thiz, RC_OBJ_WH_G *rc);
-    void (*set)(void *thiz, RC_OBJ_WH_G *rc, ...);
-    void (*setter)(void **val, ...);
-    void (*bindSetter)(Obj_Op_WH_G *op, RC_OBJ_WH_G *rc, void (*setter)(void **val, ...));
-    void (*releaser)(void *val);
-    void (*bindReleaser)(Obj_Op_WH_G *op, RC_OBJ_WH_G *rc, void (*releaser)(void* val));
-    void (*toString)(void *thiz, RC_OBJ_WH_G *rc, char *str);
-    short (*compare)(void *thiz, RC_OBJ_WH_G *rc, void *aVal);
-    void (*freeOp)(Obj_Op_WH_G *op, RC_OBJ_WH_G *rc);
+    void (*destory)(Object_WH_G *thiz, int *rc);
+    void *(*clone)(Object_WH_G *thiz, int *rc);
+    void (*set)(Object_WH_G *thiz,
+                int *rc,
+                ... //void* vals[] or void* val
+                );
+    void (*toString)(Object_WH_G *thiz, int *rc, char *str);
+    short (*compare)(Object_WH_G *thiz, int *rc, Object_WH_G *aVal);
+
+    void (*releaser)(Object_WH_G *obj);
+    void (*setter)(Object_WH_G *pVal, ...); //void* vals[] or void* val
 };
 
-struct __OBJECT_13S13_163_COM_{ //object
-    void *val; //data
-    Obj_Op_WH_G *op; //operation
+struct __OBJECT_13S13_163_COM_ {
+    void *data;
+    Object_Op_WH_G *obj_op;
 };
 
-Object_WH_G *Object_WH_G_getInst(RC_OBJ_WH_G *rc);
-void Object_WH_G_Init(Object_WH_G *obj, RC_OBJ_WH_G *rc);
-Obj_Op_WH_G *Object_WH_G_getOpInst(RC_OBJ_WH_G *rc);
-void Obj_Op_WH_G_Init(Obj_Op_WH_G *op, RC_OBJ_WH_G *rc);
-void Object_WH_G_bindOperation(Object_WH_G *obj, Obj_Op_WH_G *op, RC_OBJ_WH_G *rc);
+Object_WH_G *Obj_WH_G_getInst(int *rc);
+void Obj_WH_G_Init(Object_WH_G *obj, int *rc);
+void Obj_Op_WH_G_Init(Object_Op_WH_G *op, int *rc);
 
 #endif // OBJECT_WH_G_H
